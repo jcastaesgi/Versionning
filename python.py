49472d83fadd8d0f85872eda2688fc7ee65a2792
Jérôme Casta <jcasta1@myges.fr>
2021-05-20 18:43:56 +0200

    ajout du début de code - gaussian elimination

--- python.py
+++ python.py
@@ -1,1 +1,21 @@
-ceci est mon code python
+## Fonction gaussian elimination - python ##
+
+"""
+Gaussian elimination method for solving a system of linear equations.
+Gaussian elimination - https://en.wikipedia.org/wiki/Gaussian_elimination
+"""
+
+
+import numpy as np
+
+
+def retroactive_resolution(coefficients: np.matrix, vector: np.ndarray) -> np.ndarray:
+    """
+    This function performs a retroactive linear system resolution
+        for triangular matrix
+    Examples:
+        2x1 + 2x2 - 1x3 = 5         2x1 + 2x2 = -1
+        0x1 - 2x2 - 1x3 = -7        0x1 - 2x2 = -1
+        0x1 + 0x2 + 5x3 = 15
+
+
